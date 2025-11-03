3a8828d0527fa2ab8ef25a98c3716ad6e2751240
Bipul Hossain <bestbipul@gmail.com>
2025-11-03 23:30:32 +0600

    Preaparing Backend

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("object");
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect("mongodb://localhost:27017/dollshop")
+  .then(() => console.log("Mongodb Database Conection Successfully"))
+  .catch((err) =>
+    console.error("Could not connect to the Mongodb Database", err)
+  );
+
+app.get("/", (req, res) => {
+  res.send(" Server runndig succefully");
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`App listening on Port ${port}`));
